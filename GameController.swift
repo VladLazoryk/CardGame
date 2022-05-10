fa1ac27ee1794c03f2e25f86b42132eb4f1fe9f9
Vlad Lazoryk <50370915+VladLazoryk@users.noreply.github.com>
2022-05-10 23:32:26 +0300

    Create GameController.swift

---
+++ GameController.swift
@@ -0,0 +1,173 @@
+import UIKit
+import AFNetworking
+
+class GameController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var timer: UILabel!
+
+    var counter = 0
+    var time = Timer()
+    
+    fileprivate let sectionInsets = UIEdgeInsets(top: 20.0, left: 20.0, bottom: 20.0, right: 20.0)
+    
+    let game = MemoryGame()
+    var cards = [Card]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        game.delegate = self
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.isHidden = true
+        
+        APIClient.shared.getCardImages { (cardsArray, error) in
+            if let _ = error {
+                // show alert
+            }
+            
+            self.cards = cardsArray!
+            self.setupNewGame()
+        }
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        if game.isPlaying {
+            resetGame()
+        }
+    }
+    
+    func setupNewGame() {
+        cards = game.newGame(cardsArray: self.cards)
+        collectionView.reloadData()
+    }
+    
+    func resetGame() {
+        game.restartGame()
+        setupNewGame()
+    }
+    
+    @IBAction func onStartGame(_ sender: Any) {
+        collectionView.isHidden = false
+        
+        counter = 0
+        timer.text = "0:00"
+        
+        time.invalidate()
+        time = Timer.scheduledTimer(
+            timeInterval: 1.0,
+            target: self,
+            selector: #selector(timerAction),
+            userInfo: nil,
+            repeats: true
+        )
+    }
+    
+    @objc func timerAction() {
+        counter += 1
+        timer.text = "0:\(counter)"
+    }
+}
+
+// MARK: - CollectionView Delegate Methods
+extension GameController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cards.count 
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath) as! CardCell
+        cell.showCard(false, animted: false)
+        
+        guard let card = game.cardAtIndex(indexPath.item) else { return cell }
+        cell.card = card
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath) as! CardCell
+        
+        if cell.shown { return }
+        game.didSelectCard(cell.card)
+        
+        collectionView.deselectItem(at: indexPath, animated:true)
+    }
+}
+
+// MARK: - MemoryGameProtocol Methods
+extension GameController: MemoryGameProtocol {
+    
+    func memoryGameDidStart(_ game: MemoryGame) {
+        collectionView.reloadData()
+    }
+    
+    func memoryGame(_ game: MemoryGame, showCards cards: [Card]) {
+        for card in cards {
+            guard let index = game.indexForCard(card) else { continue }
+            let cell = collectionView.cellForItem(at: IndexPath(item: index, section:0)) as! CardCell
+            cell.showCard(true, animted: true)
+        }
+    }
+    
+    func memoryGame(_ game: MemoryGame, hideCards cards: [Card]) {
+        for card in cards {
+            guard let index = game.indexForCard(card) else { continue }
+            let cell = collectionView.cellForItem(at: IndexPath(item: index, section:0)) as! CardCell
+            cell.showCard(false, animted: true)
+        }
+    }
+    
+    func memoryGameDidEnd(_ game: MemoryGame) {
+        time.invalidate()
+        
+        let alertController = UIAlertController(
+            title: defaultAlertTitle,
+            message: defaultAlertMessage,
+            preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "Nah", style: .cancel) { [weak self] (action) in
+            self?.collectionView.isHidden = true
+        }
+        let playAgainAction = UIAlertAction(title: "Dale!", style: .default) { [weak self] (action) in
+            self?.collectionView.isHidden = true
+            self?.resetGame()
+        }
+        
+        alertController.addAction(cancelAction)
+        alertController.addAction(playAgainAction)
+        
+        self.present(alertController, animated: true) { }
+        
+        resetGame()
+    }
+}
+
+extension GameController: UICollectionViewDelegateFlowLayout {
+    
+    // Collection view flow layout setup
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let paddingSpace = Int(sectionInsets.left) * 4
+        let availableWidth = Int(view.frame.width) - paddingSpace
+        let widthPerItem = availableWidth / 4
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
+}
