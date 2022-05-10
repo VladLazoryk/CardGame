b8ec63bd26365be9edb2ba6e45c4872968f5b40a
Vlad Lazoryk <50370915+VladLazoryk@users.noreply.github.com>
2022-05-10 23:31:39 +0300

    Create Constants.swift

---
+++ Constants.swift
@@ -0,0 +1,8 @@
+let keyArtworkURL = "artwork_url"
+let keyTracks = "tracks"
+let keyID = "id"
+
+// MARK: - Default Values
+let defaultGridSize = 4 * 4
+let defaultAlertTitle = "Good job, Memory Master!"
+let defaultAlertMessage = "Want to play again?"
