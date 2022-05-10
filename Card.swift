7c18ad7c49c97425bbfffa85a43ba8ee5cc030b4
Vlad Lazoryk <50370915+VladLazoryk@users.noreply.github.com>
2022-05-10 23:29:28 +0300

    Create Card.swift

---
+++ Card.swift
@@ -0,0 +1,43 @@
+import UIKit
+import SwiftyJSON
+
+class Card {
+
+    // MARK: - Properties
+    
+    var id: String
+    var shown: Bool = false
+    var artworkURL: UIImage!
+    
+    static var allCards = [Card]()
+
+    init(card: Card) {
+        self.id = card.id
+        self.shown = card.shown
+        self.artworkURL = card.artworkURL
+    }
+    
+    init(image: UIImage) {
+        self.id = NSUUID().uuidString
+        self.shown = false
+        self.artworkURL = image
+    }
+    
+    // MARK: - Methods
+    
+    func equals(_ card: Card) -> Bool {
+        return (card.id == id)
+    }
+    
+    func copy() -> Card {
+        return Card(card: self)
+    }
+}
+
+extension Array {
+    mutating func shuffle() {
+        for _ in 0...self.count {
+            sort { (_,_) in arc4random() < arc4random() }
+        }
+    }
+}
